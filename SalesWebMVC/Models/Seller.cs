bdf285499945d5489eef21c091b78084be44c6c2
KauanMir <kauanpqs@gmail.com>
2022-01-26 20:04:17 -0300

    Outras entidades e segunda migration

--- SalesWebMVC/Models/Seller.cs
+++ SalesWebMVC/Models/Seller.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace SalesWebMVC.Models
 {
@@ -10,6 +12,33 @@ namespace SalesWebMVC.Models
         public DateTime BirthDate { get; set; }
         public double BaseSalary { get; set; }
         public Department Department { get; set; }
+        public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
 
+        public Seller()
+        {
+        }
+
+        public Seller(int id, string name, string email, DateTime birthDate, double baseSalary, Department department)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+            BirthDate = birthDate;
+            BaseSalary = baseSalary;
+            Department = department;
+        }
+        public void AddSaler(SalesRecord sr)
+        {
+            Sales.Add(sr);
+        }
+        public void RemoveSale(SalesRecord sr)
+        {
+            Sales.Remove(sr);
+        }
+
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            return Sales.Where(sr => sr.Date >= initial && sr.Date <= final).Sum(sr => sr.Amount);
+        }
     }
 }
