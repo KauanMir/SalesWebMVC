ce0e1081c9662fd9c96b28f36c4c93d4d7f142b7
KauanMir <kauanpqs@gmail.com>
2022-01-28 19:06:36 -0300

    SellerService e FindAll síncrono

---
+++ SalesWebMVC/Controllers/SellersController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SalesWebMVC.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMVC.Controllers
+{
+    public class SellersController : Controller
+    {
+        private readonly SellerService _sellerService;
+        public SellersController(SellerService sellerService)
+        {
+            _sellerService = sellerService;
+        }
+        public IActionResult Index()
+        {
+            var list = _sellerService.FindAll();
+            return View(list);
+        }
+    }
+}
