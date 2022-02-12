0f36e007f8257db2e70ac062859348f0cdb8f73a
KauanMir <kauanpqs@gmail.com>
2022-02-12 10:48:30 -0300

    Localização e formatação de números e datas

--- SalesWebMVC/Startup.cs
+++ SalesWebMVC/Startup.cs
@@ -12,6 +12,8 @@ using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using SalesWebMVC.Data;
 using SalesWebMVC.Services;
+using Microsoft.AspNetCore.Localization;
+using System.Globalization;
 
 namespace SalesWebMVC
 {
@@ -52,6 +54,15 @@ namespace SalesWebMVC
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, SeedingService seedingService)
         {
+            var enUS = new CultureInfo("en-US");
+            var localizationOptions = new RequestLocalizationOptions
+            {
+                DefaultRequestCulture = new RequestCulture(enUS),
+                SupportedCultures = new List<CultureInfo> { enUS },
+                SupportedUICultures = new List<CultureInfo> { enUS }
+            };
+            app.UseRequestLocalization(localizationOptions);
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
